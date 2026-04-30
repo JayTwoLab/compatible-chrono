c03d7e265d28e0821d52cfc4b8104ea41e46cd0f
Jay Two <jaytwo@jaytwo.kr>
2026-04-30 22:16:40 +0900

    testing

--- compatible_chrono.hpp
+++ compatible_chrono.hpp
@@ -2,21 +2,14 @@
 
 #if __cplusplus >= 202002L
     // C++20 and later: using standard chrono
-    #include <chrono>
-    #include <format> 
 
-    namespace compatible_chrono = std::chrono;
+    #include <chrono> // c++20 chrono library header
+    #include <format> // c++20 format library header
+
+    namespace compatible_chrono = std::chrono; // Alias for std::chrono
     
     // C++20 supports output with std::format
     #define CHRONO_FORMAT(fmt, tp) std::format("{:" fmt "}", tp)
-
-    // #if defined(_MSC_VER)
-        // MSVC
-        // #define CHRONO_FORMAT(fmt, tp) std::format(fmt, tp)
-    // #else
-        // GCC/Clang
-        // #define CHRONO_FORMAT(fmt, tp) std::format("{:" fmt "}", tp)
-    // #endif    
 
 #else
     // C++17 (Less than C++20): Using Howard Hinnant's date library
@@ -25,13 +18,14 @@
     #include <date/tz.h>  // time zone support (optional, include if needed)
     #include <date/iso_week.h> // ISO 8601 week date support (year, week number, weekday)
 
-	// Unused calendar systems (comment out if not needed)
+    // Unused calendar systems (comment out if not needed)
     // #include <date/julian.h> // Julian calendar support
     // #include <date/islamic.h> // Islamic calendar support
     // #include <date/solar_hijri.h> // Solar Hijri calendar support
 
-    namespace compatible_chrono = date;
+    namespace compatible_chrono = date; // Alias for date library namespace
 
     // Using the format function of date.h
     #define CHRONO_FORMAT(fmt, tp) date::format(fmt, tp)
+
 #endif
