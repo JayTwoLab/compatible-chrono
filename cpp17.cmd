cd738cc1080b8cab13c7371a2d32b984ac6890d2
Jay Two <jaytwo@jaytwo.kr>
2026-05-03 16:24:37 +0900

    testing

--- cpp17.cmd
+++ cpp17.cmd
@@ -1,3 +1,8 @@
+@REM build gtest
 rmdir /s /q build-cpp17
 cmake -B build-cpp17 -S . -DCMAKE_CXX_STANDARD=17
 cmake --build build-cpp17
+
+@REM run gtest
+build-cpp17\Debug\test_compatible_chrono
+
--- cpp20.cmd
+++ cpp20.cmd
@@ -1,4 +1,10 @@
+
+@REM build gtest
 rmdir /s /q build-cpp20
 cmake -B build-cpp20 -S . -DCMAKE_CXX_STANDARD=20
 cmake --build build-cpp20
+
+@REM run gtest
+.\build-cpp20\Debug\test_compatible_chrono
+
 
