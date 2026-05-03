fa1d5eb0caff19d573796603a53eb71e74784e41
Jay Two <jaytwo@jaytwo.kr>
2026-05-03 14:23:35 +0900

    testing

--- cpp17.sh
+++ cpp17.sh
@@ -1,3 +1,8 @@
+# build gtest 
 rm -rf build-cpp17
 cmake -B build-cpp17 -S . -DCMAKE_CXX_STANDARD=17
 cmake --build build-cpp17
+
+# execute gtest
+./build-cpp17/test_compatible_chrono
+
--- cpp20.sh
+++ cpp20.sh
@@ -1,4 +1,8 @@
+# build gtest
 rm -rf build-cpp20
 cmake -B build-cpp20 -S . -DCMAKE_CXX_STANDARD=20
 cmake --build build-cpp20
+
+# execute gtest
+./build-cpp20/test_compatible_chrono
 
